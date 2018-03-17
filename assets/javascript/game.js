783c9edacd968b75e237c5691a9089f0ac8167aa
kavitha <kavitha.veluchamy@gmail.com>
2018-03-17 08:49:25 -0400

    Cleared all the console logs

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -12,42 +12,32 @@ $(document).ready(function () {
     function generate() {
         randomNumber = Math.floor(Math.random() * (100 - 1) + 1);
         $("#random").html("Your random number is : " + randomNumber);
-        console.log("user's Random Number:" + randomNumber);
 
         crystal1 = Math.floor(Math.random() * (10 - 1) + 1);
         crystal2 = Math.floor(Math.random() * (10 - 1) + 1);
         crystal3 = Math.floor(Math.random() * (10 - 1) + 1);
         crystal4 = Math.floor(Math.random() * (10 - 1) + 1);
-
-        console.log("crystal 1:" + crystal1);
-        console.log("crystal 2:" + crystal2);
-        console.log("crystal 3:" + crystal3);
-        console.log("crystal 4:" + crystal4);
         result = 0;
     }
 
     $("#img1").on("click", function () {
-        console.log("image1 clicked");
         result += crystal1;
         checkResult();
         $("#score").html("Score : " + result);
     });
     $("#img2").click(function () {
-        console.log("image2 clicked");
         result += crystal2;
         checkResult();
         $("#score").html("Score : " + result);
 
     });
     $("#img3").click(function () {
-        console.log("image2 clicked");
         result += crystal3;
         checkResult();
         $("#score").html("Score : " + result);
 
     });
     $("#img4").click(function () {
-        console.log("image2 clicked");
         result += crystal4;
         checkResult();
         $("#score").html("Score : " + result);
@@ -55,9 +45,6 @@ $(document).ready(function () {
     });
 
     function checkResult() {
-        console.log("Inside check result");
-        console.log("random: "+randomNumber);
-        console.log("Result: "+result);
         if (result == randomNumber) {
             alert("You Win!!!");
             wins += 1;
